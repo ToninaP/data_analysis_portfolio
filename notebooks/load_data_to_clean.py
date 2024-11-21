ca01d57a487e00f7fd44b605d00f5181e914e6a5
ToninaP <84412989+ToninaP@users.noreply.github.com>
2024-11-21 14:14:22 -0500

    working on cleaning data

---
+++ notebooks/load_data_to_clean.py
@@ -0,0 +1,137 @@
+import pandas as pd
+from flatten_json import flatten
+import json
+import re
+
+
+def load_data_to_clean():
+    # Load csv files and json files that do not require flattening or parsing
+    data_paths_standard = [
+        "/Users/antoninalightfoot/Library/Mobile Documents/com~apple~CloudDocs/Documents/TLU/Data analysis/data/Met/MetObjects.csv",
+        "/Users/antoninalightfoot/Library/Mobile Documents/com~apple~CloudDocs/Documents/TLU/Data analysis/data/Reina Sofia/raw_data/reina_sofia3.csv",
+        "/Users/antoninalightfoot/Documents/GitHub/museum_personal/data/Tate/tate_raw.csv",
+        "/Users/antoninalightfoot/Library/Mobile Documents/com~apple~CloudDocs/Documents/TLU/Data analysis/data/Centre Pompidou/merged_file.json",
+        "/Users/antoninalightfoot/Documents/GitHub/museum_personal/data/Moma/Artworks.csv",
+        "/Users/antoninalightfoot/Documents/GitHub/museum_personal/data/Whitneymuseum/whitney_data_raw.csv",
+        "/Users/antoninalightfoot/Documents/GitHub/museum_personal/data/National Gallery (DC)/national_gallery_raw.csv",
+    ]
+    raw_data = []
+    dataset_names = [
+        "met",
+        "reina_sofia",
+        "tate",
+        "pompidou",
+        "moma",
+        "whitney",
+        "national_gallery",
+    ]
+    for df, dataset_name in zip(data_paths_standard, dataset_names):
+        # Step 1: Check if '.json' exists in the input
+        if ".json" in df:
+            data = pd.read_json(df)
+        elif ".csv" in df:
+            data = pd.read_csv(df, on_bad_lines="skip", low_memory=False)
+        else:
+            print(f"Cannot load {df}")
+            continue
+
+        raw_data.append(data)
+        print(f"Loaded {dataset_name} with shape: {data.shape}")
+
+    # Load datasets that require flattening
+    data_paths_flattening = [
+        "/Users/antoninalightfoot/Library/Mobile Documents/com~apple~CloudDocs/Documents/TLU/Data analysis/data/Kiasma/APIexample-master/dataset.json",  # Kiasma
+        "/Users/antoninalightfoot/Documents/GitHub/museum_personal/data/SMK/smk_all_da.json",
+        "/Users/antoninalightfoot/Library/Mobile Documents/com~apple~CloudDocs/Documents/TLU/Data analysis/data/Kiasma/APIexample-master/dataset.json",  # Ateneum
+    ]
+    flat_data = []
+    dictionary_data = []
+    for df in data_paths_flattening:
+        with open(df, "r") as file:
+            data = json.load(file)
+            dictionary_data.append(data)
+
+    for df in dictionary_data:
+        flattened_data = [flatten(record) for record in df]
+        data = pd.DataFrame(flattened_data)
+        flat_data.append(data)
+        print(f"Loaded flattened data with shape: {data.shape}")
+
+    # Separate kiasma and ateneum
+    flat_data[0] = flat_data[0][
+        flat_data[0]["responsibleOrganisation"]
+        == "Kansallisgalleria / Nykytaiteen museo Kiasma"
+    ]
+    flat_data[2] = flat_data[2][
+        flat_data[2]["responsibleOrganisation"]
+        == "Kansallisgalleria / Ateneumin taidemuseo"
+    ]
+
+    new_datasets = ["kiasma", "smk", "ateneum"]
+    dataset_names.extend(new_datasets)
+    raw_data.extend(flat_data)
+
+    # File path to the raw JSON data
+    data_path = "/Users/antoninalightfoot/Documents/GitHub/museum_personal/data/Queensland/raw_data.json"
+
+    # Load the JSON data
+    with open(data_path, "r") as file:
+        data = json.load(file)
+
+    # Extract the column names from the "fields" part of the JSON
+    columns = [field["id"] for field in data["fields"]]
+
+    # Extract the records (the data)
+    records = data["records"]
+
+    # Create the DataFrame
+    queensland = pd.DataFrame(records, columns=columns)
+    raw_data.append(queensland)
+    dataset_names.append("queensland")
+    print(f"Loaded queensland with shape: {queensland.shape}")
+
+    # Prepare queensland for analysis
+    def assign_dates(row, column_name):
+        text = row[column_name]
+
+        # Use regular expressions to find patterns for years
+        years = re.findall(r"\b\d{4}\b", text)  # Find all 4-digit numbers
+
+        # If exactly one year is found, it is the birth year
+        if len(years) == 1:
+            row["birth_date"] = int(years[0])
+            row["death_date"] = None  # No death date if only one year is found
+        # If exactly two years are found, assign the first to birth_date and the second to death_date
+        elif len(years) >= 2:
+            row["birth_date"] = int(years[0])
+            row["death_date"] = int(years[1])
+        else:
+            # If no years or more than two years are found, set both to None
+            row["birth_date"] = None
+            row["death_date"] = None
+
+        return row
+
+    raw_data[10]["Artist"] = raw_data[10]["Person"].str.split("\n").str.get(0)
+    raw_data[10]["Nationality"] = (
+        raw_data[10]["Person"].str.split("\n").str.get(-1).str.split("1").str.get(0)
+    )
+    raw_data[10]["birth_date"] = None
+    raw_data[10]["death_date"] = None
+    raw_data[10] = raw_data[10].apply(assign_dates, axis=1, column_name="Person")
+
+    # Removing duplicate columns created with flattening json
+    raw_data[7] = raw_data[7].loc[:, ~raw_data[7].columns.str.startswith("keywords")]
+    raw_data[7] = raw_data[7].loc[:, ~raw_data[7].columns.str.startswith("multimedia")]
+    raw_data[7]["artist_name"] = (
+        raw_data[7]["people_0_firstName"] + " " + raw_data[7]["people_0_familyName"]
+    )
+
+    # Removing duplicate columns created with flattening json
+    raw_data[9] = raw_data[9].loc[:, ~raw_data[9].columns.str.startswith("keywords")]
+    raw_data[9] = raw_data[9].loc[:, ~raw_data[9].columns.str.startswith("multimedia")]
+    raw_data[9]["artist_name"] = (
+        raw_data[9]["people_0_firstName"] + " " + raw_data[9]["people_0_familyName"]
+    )
+
+    return raw_data, dataset_names
---
+++ notebooks/load_nationality_tags.py
@@ -0,0 +1,147 @@
+def load_nationality_tags():
+
+    nationality_tags = [
+        ["american", "américaine", "usa", "united states"],  # United States
+        ["allemande", "germany", "german", "tysk"],  # Germany
+        ["canadian", "canadienne", "canada"],  # Canada
+        ["japanese", "japonaise", "japan", "japansk"],  # Japan
+        ["mexican", "mexicaine", "mexico"],  # Mexico
+        ["indian", "india"],  # India
+        ["française", "france", "french", "fransk"],  # France
+        ["russe", "russia", "soviétique", "russian"],  # Russia
+        ["suisse", "switzerland", "swiss", "schweizisk"],  # Switzerland
+        [
+            "britannique",
+            "united kingdom",
+            "england",
+            "scotland",
+            "english",
+            "engelsk",
+        ],  # United Kingdom
+        ["hongroise", "hungary", "hungarian"],  # Hungary
+        ["argentine", "argentina", "argentinian"],  # Argentina
+        ["polonaise", "poland", "polish", "polsk"],  # Poland
+        ["yougoslave", "yugoslavia"],  # Yugoslavia
+        ["suédoise", "sweden", "swedish", "svensk"],  # Sweden
+        ["italienne", "italy", "italian", "italiensk"],  # Italy
+        ["espagnole", "spain", "spanish"],  # Spain
+        ["brésilienne", "brazil", "brazilian"],  # Brazil
+        ["égyptienne", "egypt"],  # Egypt
+        ["vénézuélienne", "venezuela"],  # Venezuela
+        ["israélienne", "israel"],  # Israel
+        ["cubaine", "cuba", "cuban"],  # Cuba
+        ["iranienne", "iran", "iranian"],  # Iran
+        ["algérienne", "algeria", "algerian"],  # Algeria
+        ["ukrainienne", "ukraine", "ukrainian"],  # Ukraine
+        ["roumaine", "romania", "romanian", "rumænsk"],  # Romania
+        ["belge", "belgium", "belgian", "belgisk"],  # Belgium
+        [
+            "néerlandaise",
+            "netherlands",
+            "holland",
+            "nederlandsk",
+            "hollandsk",
+        ],  # Netherlands
+        ["autrichienne", "austria", "austrian", "østrigsk"],  # Austria
+        ["irlandaise", "ireland", "irish"],  # Ireland
+        ["portugaise", "portugal", "portugese"],  # Portugal
+        ["jordanienne", "jordan"],  # Jordan
+        ["australienne", "australia", "australian"],  # Australia
+        ["croate", "croatia", "croatian", "kroatisk"],  # Croatia
+        ["tchèque", "czechia", "czech republic", "tchécoslovaque"],  # Czech Republic
+        ["albanaise", "albania"],  # Albania
+        ["sud-africaine", "south africa"],  # South Africa
+        ["irakienne", "iraq"],  # Iraq
+        ["libanaise", "lebanon", "lebanese"],  # Lebanon
+        ["estonienne", "estonia"],  # Estonia
+        ["kosovare", "kosovo"],  # Kosovo
+        ["marocaine", "morocco"],  # Morocco
+        ["ivoirienne", "ivory coast"],  # Ivory Coast
+        ["arménienne", "armenia"],  # Armenia
+        ["chinoise", "china", "chinese"],  # China
+        ["vietnamienne", "vietnam"],  # Vietnam
+        ["guatemala"],  # Guatemala
+        ["bermuda"],  # bermuda
+        ["new zealand"],  # new zealand
+        ["turkey", "turkish"],  # turkey
+        ["kirghiztan"],  # Kyrgyzstan
+        ["uruguay"],  # uruguay
+        ["nigeria", "nigeriansk"],  # nigeria
+        ["paraguay"],  # paraguay
+        ["serbia", "serbisk"],  # serbia
+        ["peru"],  # peru
+        ["chile"],  # chile
+        ["palestine"],  # palestine
+        ["denmark"],  # denmark
+        ["bosnia-herzegovina"],  #'bosnia-herzegovina'
+        ["norway"],  # norway
+        ["finnish"],  # finland
+    ]
+
+    country_name = [
+        "United States",
+        "Germany",
+        "Canada",
+        "Japan",
+        "Mexico",
+        "India",
+        "France",
+        "Russia",
+        "Switzerland",
+        "United Kingdom",
+        "Hungary",
+        "Argentina",
+        "Poland",
+        "Yugoslavia",
+        "Sweden",
+        "Italy",
+        "Spain",
+        "Brazil",
+        "Egypt",
+        "Venezuela",
+        "Israel",
+        "Cuba",
+        "Iran",
+        "Algeria",
+        "Ukraine",
+        "Romania",
+        "Belgium",
+        "Netherlands",
+        "Stateless",
+        "Austria",
+        "Ireland",
+        "Portugal",
+        "Jordan",
+        "Australia",
+        "Croatia",
+        "Czech Republic",
+        "Albania",
+        "South Africa",
+        "Iraq",
+        "Lebanon",
+        "Estonia",
+        "Kosovo",
+        "Morocco",
+        "Ivory Coast",
+        "Armenia",
+        "China",
+        "Vietnam",
+        "Guatemala",
+        "Bermuda",
+        "New Zealand",
+        "Turkey",
+        "Kyrgyzstan",
+        "Uruguay",
+        "Nigeria",
+        "Paraguay",
+        "Serbia",
+        "Peru",
+        "Chile",
+        "Palestine",
+        "Denmark",
+        "Bosnia-Herzegovina",
+        "Norway",
+        "Finland",
+    ]
+
+    return nationality_tags, country_name
